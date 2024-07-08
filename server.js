3c84c51554c488037ceb7160832a460572745ff6
Shyamak Seth <sethshyamak@gmail.com>
2024-07-08 11:34:53 +0530

    changed running status

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ app.use(express.json({limit: '1mb'}))
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.end('Server running')
+})
 app.use('/login', loginRouter)
 app.use('/register', regRouter)
 app.use('/media', mediaRouter)
 app.get('/testapi', (req, res) => {
     const path = require('path')
     return res.sendFile(path.join(__dirname, 'test.html'))
-})
-app.get('/info', (req, res) => {
-    res.end('Server running')
 })
 
 mongoose.connect(process.env.MONGO_URI, console.log('MONGO CONNECTED'))
